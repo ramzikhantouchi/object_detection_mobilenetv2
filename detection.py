000400d218bbc9adf40452224239f7a06d7acaa3
Ramzi Khantouchi <47026044+ramzikhantouchi@users.noreply.github.com>
2024-06-19 15:58:48 +0100

    Add files via upload

---
+++ detection.py
@@ -0,0 +1,144 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = ''
+
+import cv2
+import numpy as np
+import tensorflow as tf
+
+# Load the pre-trained model
+model_path = 'detector_model/saved_model'
+detect_fn = tf.saved_model.load(model_path)
+
+# Load the COCO labels
+category_index = {
+    1: {'id': 1, 'name': 'person'},
+    2: {'id': 2, 'name': 'bicycle'},
+    3: {'id': 3, 'name': 'car'},
+    4: {'id': 4, 'name': 'motorcycle'},
+    5: {'id': 5, 'name': 'airplane'},
+    6: {'id': 6, 'name': 'bus'},
+    7: {'id': 7, 'name': 'train'},
+    8: {'id': 8, 'name': 'truck'},
+    9: {'id': 9, 'name': 'boat'},
+    10: {'id': 10, 'name': 'traffic light'},
+    11: {'id': 11, 'name': 'fire hydrant'},
+    13: {'id': 13, 'name': 'stop sign'},
+    14: {'id': 14, 'name': 'parking meter'},
+    15: {'id': 15, 'name': 'bench'},
+    16: {'id': 16, 'name': 'bird'},
+    17: {'id': 17, 'name': 'cat'},
+    18: {'id': 18, 'name': 'dog'},
+    19: {'id': 19, 'name': 'horse'},
+    20: {'id': 20, 'name': 'sheep'},
+    21: {'id': 21, 'name': 'cow'},
+    22: {'id': 22, 'name': 'elephant'},
+    23: {'id': 23, 'name': 'bear'},
+    24: {'id': 24, 'name': 'zebra'},
+    25: {'id': 25, 'name': 'giraffe'},
+    27: {'id': 27, 'name': 'backpack'},
+    28: {'id': 28, 'name': 'umbrella'},
+    31: {'id': 31, 'name': 'handbag'},
+    32: {'id': 32, 'name': 'tie'},
+    33: {'id': 33, 'name': 'suitcase'},
+    34: {'id': 34, 'name': 'frisbee'},
+    35: {'id': 35, 'name': 'skis'},
+    36: {'id': 36, 'name': 'snowboard'},
+    37: {'id': 37, 'name': 'sports ball'},
+    38: {'id': 38, 'name': 'kite'},
+    39: {'id': 39, 'name': 'baseball bat'},
+    40: {'id': 40, 'name': 'baseball glove'},
+    41: {'id': 41, 'name': 'skateboard'},
+    42: {'id': 42, 'name': 'surfboard'},
+    43: {'id': 43, 'name': 'tennis racket'},
+    44: {'id': 44, 'name': 'bottle'},
+    46: {'id': 46, 'name': 'wine glass'},
+    47: {'id': 47, 'name': 'cup'},
+    48: {'id': 48, 'name': 'fork'},
+    49: {'id': 49, 'name': 'knife'},
+    50: {'id': 50, 'name': 'spoon'},
+    51: {'id': 51, 'name': 'bowl'},
+    52: {'id': 52, 'name': 'banana'},
+    53: {'id': 53, 'name': 'apple'},
+    54: {'id': 54, 'name': 'sandwich'},
+    55: {'id': 55, 'name': 'orange'},
+    56: {'id': 56, 'name': 'broccoli'},
+    57: {'id': 57, 'name': 'carrot'},
+    58: {'id': 58, 'name': 'hot dog'},
+    59: {'id': 59, 'name': 'pizza'},
+    60: {'id': 60, 'name': 'donut'},
+    61: {'id': 61, 'name': 'cake'},
+    62: {'id': 62, 'name': 'chair'},
+    63: {'id': 63, 'name': 'couch'},
+    64: {'id': 64, 'name': 'potted plant'},
+    65: {'id': 65, 'name': 'bed'},
+    67: {'id': 67, 'name': 'dining table'},
+    70: {'id': 70, 'name': 'toilet'},
+    72: {'id': 72, 'name': 'tv'},
+    73: {'id': 73, 'name': 'laptop'},
+    74: {'id': 74, 'name': 'mouse'},
+    75: {'id': 75, 'name': 'remote'},
+    76: {'id': 76, 'name': 'keyboard'},
+    77: {'id': 77, 'name': 'cell phone'},
+    78: {'id': 78, 'name': 'microwave'},
+    79: {'id': 79, 'name': 'oven'},
+    80: {'id': 80, 'name': 'toaster'},
+    81: {'id': 81, 'name': 'sink'},
+    82: {'id': 82, 'name': 'refrigerator'},
+    84: {'id': 84, 'name': 'book'},
+    85: {'id': 85, 'name': 'clock'},
+    86: {'id': 86, 'name': 'vase'},
+    87: {'id': 87, 'name': 'scissors'},
+    88: {'id': 88, 'name': 'teddy bear'},
+    89: {'id': 89, 'name': 'hair drier'},
+    90: {'id': 90, 'name': 'toothbrush'}
+}
+
+
+# Initialize webcam
+cap = cv2.VideoCapture(1)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to grab frame")
+        break
+
+   
+    input_tensor = tf.convert_to_tensor(np.expand_dims(frame, 0), dtype=tf.uint8) 
+
+   
+    detections = detect_fn.signatures["serving_default"](input_tensor)
+
+    
+    for i in range(int(detections['num_detections'][0])):
+        score = detections['detection_scores'][0][i].numpy()
+        if score < 0.5:
+            continue
+
+        bbox = detections['detection_boxes'][0][i].numpy()
+        class_id = int(detections['detection_classes'][0][i].numpy())
+
+        if class_id in category_index:
+            
+            y_min, x_min, y_max, x_max = bbox
+            h, w, _ = frame.shape
+            start_point = (int(x_min * w), int(y_min * h))
+            end_point = (int(x_max * w), int(y_max * h))
+
+            
+            cv2.rectangle(frame, start_point, end_point, (255, 0, 0), 2)
+
+            
+            label = f"{category_index[class_id]['name']}: {int(score * 100)}%"
+            cv2.putText(frame, label, (start_point[0], start_point[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+
+    
+    cv2.imshow('Object Detection', frame)
+
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
